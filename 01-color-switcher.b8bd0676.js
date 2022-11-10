f2cf6b4ebc787f7fec16bc9853187598658b33bf
Reshte <107108359+Reshte@users.noreply.github.com>
2022-11-10 12:16:23 +0000

    Deploying to gh-pages from @ Reshte/goit-js-hw-09@990bc72039f91960ae61d2134f5689b931186b44 🚀

---
+++ 01-color-switcher.b8bd0676.js
@@ -0,0 +1,2 @@
+function t(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}t();let n=null;const e={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};e.startBtn.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=t(),e.startBtn.disabled=!0}),1e3)})),e.stopBtn.addEventListener("click",(function(){e.startBtn.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.b8bd0676.js.map
