990bc72039f91960ae61d2134f5689b931186b44
Reshte <107108359+Reshte@users.noreply.github.com>
2022-11-10 14:15:52 +0200

    task -1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона
+// < body > на случайное значение используя инлайн стиль.При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
+// Учти, на кнопку «Start» можно нажать бесконечное количество раз.Сделай так,
+// чтобы пока изменение темы запушено, кнопка «Start» была не активна(disabled).
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let backgroundColor = getRandomHexColor();
+let intervalID = null;
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.startBtn.disabled = true;
+  }, 1000);
+}
+
+function stopChangeColor() {
+  refs.startBtn.disabled = false;
+  clearInterval(intervalID);
+}
