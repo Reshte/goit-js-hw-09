647291591ead288f53ec0a564a0a6d5dd7a0340b
Reshte <107108359+Reshte@users.noreply.github.com>
2022-11-12 15:22:28 +0200

    task- 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,61 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const buttonEl = document.querySelector('button');
+const localObj = {
+  firstDelay: '',
+  delayStep: '',
+  amount: '',
+};
+
+formEl.addEventListener('input', textInput);
+
+function textInput() {
+  localObj.firstDelay = formEl.elements.delay.value;
+  localObj.delayStep = formEl.elements.step.value;
+  localObj.amount = formEl.elements.amount.value;
+  return localObj;
+}
+
+buttonEl.addEventListener('click', onclickSubmit);
+
+function onclickSubmit(evt) {
+  evt.preventDefault();
+  if (
+    formEl.elements.delay.value === '' ||
+    formEl.elements.step.value === '' ||
+    formEl.elements.amount.value === ''
+  ) {
+    Notiflix.Notify.info('Заполните все поля');
+  } else {
+    let position = 1;
+    let delay = Number(localObj.firstDelay);
+    while (localObj.amount > 0) {
+      createPromise(position, delay);
+      position += 1;
+      console.log(position);
+      delay += Number(localObj.delayStep);
+      console.log(delay);
+      localObj.amount -= 1;
+    }
+  }
+
+  formEl.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      } else {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      }
+    }, delay);
+  });
 }
